4c5c538215a3618b93167b61339b2735657ff464
Pavel Bibichenko <erhoof@outlook.com>
2020-10-05 08:48:45 +0300

    HardDrive init + hold params

--- HardDrive.cpp
+++ HardDrive.cpp
@@ -11,3 +11,68 @@ HardDrive *HardDrive::instance()
 
     return m_instance;
 }
+
+bool HardDrive::initHardDrive(HardDriveInfo &info)
+{
+    if (info.m_accessTime > 0.0)
+        m_accessTime = info.m_accessTime;
+    else
+        return false;
+
+    if (info.m_cylinders > 0)
+        m_cylinders = info.m_cylinders;
+    else
+        return false;
+
+    if (info.m_heads > 0)
+        m_heads = info.m_heads;
+    else
+        return false;
+
+    if (info.m_rotationDelay > 0.0)
+        m_rotationDelay = info.m_rotationDelay;
+    else
+        return false;
+
+    if (info.m_sectors > 0)
+        m_sectors = info.m_sectors;
+    else
+        return false;
+
+    if (info.m_transferSpeed > 0.0)
+        m_transferSpeed = info.m_transferSpeed;
+    else
+        return false;
+
+    return true;
+}
+
+float HardDrive::accessTimeValue() const
+{
+    return m_accessTime;
+}
+
+float HardDrive::rotationDelayValue() const
+{
+    return m_rotationDelay;
+}
+
+float HardDrive::transferSpeedValue() const
+{
+    return m_transferSpeed;
+}
+
+int HardDrive::cylindersCount() const
+{
+    return m_cylinders;
+}
+
+int HardDrive::sectorsCount() const
+{
+    return m_sectors;
+}
+
+int HardDrive::headsCount() const
+{
+    return m_heads;
+}
--- harddrive.h
+++ harddrive.h
@@ -1,11 +1,31 @@
 #ifndef HARDDRIVE_H
 #define HARDDRIVE_H
 
+struct HardDriveInfo
+{
+    float m_accessTime = 1;
+    float m_rotationDelay = 0.5;
+    float m_transferSpeed = 10.0;
+
+    int m_cylinders = 10;
+    int m_sectors = 30;
+    int m_heads = 1;
+};
+
 class HardDrive
 {
 public:
     static HardDrive *instance();
 
+    bool initHardDrive(HardDriveInfo &info);
+
+    float accessTimeValue() const;
+    float rotationDelayValue() const;
+    float transferSpeedValue() const;
+
+    int cylindersCount() const;
+    int sectorsCount() const;
+    int headsCount() const;
 private:
     static HardDrive *m_instance;
 
@@ -20,6 +40,8 @@ private:
     int m_cylinders = 10;
     int m_sectors = 30;
     int m_heads = 1;
+
+    void allocateSpace();
 };
 
 #endif // HARDDRIVE_H
