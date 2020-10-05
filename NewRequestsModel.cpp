f66c3a621bdd56838944d2d93a41b2e1aa790c96
Pavel Bibichenko <erhoof@outlook.com>
2020-10-05 11:09:25 +0300

    NewRequestsModel addition

---
+++ NewRequestsModel.cpp
@@ -0,0 +1,53 @@
+#include "NewRequestsModel.h"
+
+NewRequestsModel::NewRequestsModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+}
+
+int NewRequestsModel::rowCount(const QModelIndex &parent) const
+{
+    // For list models only the root node (an invalid parent) should return the list's size. For all
+    // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
+    if (parent.isValid())
+        return 0;
+
+    return static_cast<int>(m_list.size());
+}
+
+QVariant NewRequestsModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (role == Qt::DisplayRole)
+    {
+        return m_list[0].toString();
+    }
+
+    return QVariant();
+}
+
+void NewRequestsModel::addRequest(RequestModel req)
+{
+    m_list.push_back(req);
+}
+
+bool NewRequestsModel::removeRequest(uint id)
+{
+    int i=0;
+    for (auto &req : m_list)
+    {
+        if (req.id == id)
+            break;
+
+        i++;
+    }
+
+    if (id > m_list.size())
+        return false;
+    else
+        m_list.erase(m_list.begin() + id);
+
+    return true;
+}
---
+++ NewRequestsModel.h
@@ -0,0 +1,27 @@
+#ifndef NEWREQUESTSMODEL_H
+#define NEWREQUESTSMODEL_H
+
+#include <QAbstractListModel>
+#include <vector>
+
+#include "RequestModel.h"
+
+class NewRequestsModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    explicit NewRequestsModel(QObject *parent = nullptr);
+
+    // Basic functionality:
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    void addRequest(RequestModel req);
+    bool removeRequest(uint id);
+
+private:
+    std::vector<RequestModel> m_list;
+};
+
+#endif // NEWREQUESTSMODEL_H
