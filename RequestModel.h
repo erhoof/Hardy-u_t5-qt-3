40869d3992f3cafd7dd75012a7ac6af968e72ae2
Pavel Bibichenko <erhoof@outlook.com>
2020-10-10 21:06:51 +0300

    Head support

--- RequestModel.h
+++ RequestModel.h
@@ -15,6 +15,7 @@ struct RequestModel
 
     uint cylinder;
     uint sector;
+    uint head;
 
     QTime creationTime;
     QTime finishTime;
@@ -26,22 +27,34 @@ struct RequestModel
         return creationTime.secsTo(finishTime);
     }
 
+    QString operationTypeString() const
+    {
+        if (operationType == OperationType::Read)
+            return "Read";
+        else
+            return "Write";
+    }
+
     QString toString() const
     {
         if (finishTime == creationTime)
-            return QString("%1 (%2) [c%3:s%4] :: start At %5")
+            return QString("%6 - %1 (%2) [c%3:h%7:s%4] :: start At %5")
                     .arg(QString::number(id),
                          filename,
                          QString::number(cylinder),
                          QString::number(sector),
-                         creationTime.toString("hh:mm:ss.z"));
+                         creationTime.toString("hh:mm:ss.z"),
+                         operationTypeString(),
+                         QString::number(head));
         else
-            return QString("%1 (%2) [c%3:s%4] :: for %5 secs")
+            return QString("%6 - %1 (%2) [c%3:h%7:s%4] :: for %5 secs")
                     .arg(QString::number(id),
                          filename,
                          QString::number(cylinder),
                          QString::number(sector),
-                         QString::number(progressSecs()));
+                         QString::number(progressSecs()),
+                         operationTypeString(),
+                         QString::number(head));
     }
 };
 
