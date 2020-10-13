0b8a87bca805476cf76cb6899ed2b4536d7e2b93
Pavel Bibichenko <erhoof@outlook.com>
2020-10-13 11:27:16 +0300

    Sort init

--- SortedReqListModel.cpp
+++ SortedReqListModel.cpp
@@ -1,7 +1,14 @@
 #include "SortedReqListModel.h"
 
-SortedReqListModel::SortedReqListModel(QObject *parent)
-    : QAbstractListModel(parent)
+#include "NewRequestsModel.h"
+#include "harddrive.h"
+
+SortedReqListModel::SortedReqListModel(QObject *parent,
+                                       NewRequestsModel *newReqModel,
+                                       HardDrive *hardDrive)
+    : QAbstractListModel(parent),
+      m_newReqModel(newReqModel),
+      m_hardDrive(hardDrive)
 {
 }
 
@@ -12,7 +19,7 @@ int SortedReqListModel::rowCount(const QModelIndex &parent) const
     if (parent.isValid())
         return 0;
 
-    // FIXME: Implement me!
+    return m_newReqModel->listSize();
 }
 
 QVariant SortedReqListModel::data(const QModelIndex &index, int role) const
@@ -20,6 +27,38 @@ QVariant SortedReqListModel::data(const QModelIndex &index, int role) const
     if (!index.isValid())
         return QVariant();
 
-    // FIXME: Implement me!
+    if (role == Qt::DisplayRole)
+        return m_list[index.row()].toString();
+
     return QVariant();
+}
+
+int SortedReqListModel::finishedTaskID() const
+{
+    return m_finishedTaskID;
+}
+
+void SortedReqListModel::updateList()
+{
+    // TODO: use existing vector
+    m_list.assign(m_newReqModel->list().begin(), m_newReqModel->list().end());
+
+    Direction direction = m_hardDrive->curDirection();
+    int curCylinder = m_hardDrive->position().cylinder;
+
+    for (auto &i : m_newReqModel->list())
+    {
+        if (direction == Direction::Out)
+        {
+            auto part = std::partition(m_list.begin(), m_list.end(),
+                                  [&](RequestModel const &rm) {
+                    return true;
+            });
+        }
+    }
+}
+
+void SortedReqListModel::execTask()
+{
+
 }
--- SortedReqListModel.h
+++ SortedReqListModel.h
@@ -3,19 +3,41 @@
 
 #include <QAbstractListModel>
 
+#include <vector>
+#include "RequestModel.h"
+
+class NewRequestsModel;
+class HardDrive;
+
 class SortedReqListModel : public QAbstractListModel
 {
     Q_OBJECT
 
 public:
-    explicit SortedReqListModel(QObject *parent = nullptr);
+    SortedReqListModel(QObject *parent = nullptr,
+                       NewRequestsModel *newReqModel = nullptr,
+                       HardDrive *hardDrive = nullptr);
 
     // Basic functionality:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
+    int finishedTaskID() const;
+
+public slots:
+    void updateList();
+    void execTask();
+
+signals:
+    void taskFinished();
+
 private:
+    NewRequestsModel *m_newReqModel;
+    HardDrive *m_hardDrive;
+    std::vector<RequestModel> m_list;
+
+    int m_finishedTaskID;
 };
 
 #endif // SORTEDREQLISTMODEL_H
